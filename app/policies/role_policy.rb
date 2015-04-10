18325d5876c71bfba898f1a243bb5633b6ba3b48
Ray Chan <ray.chan@laxino.com>
2015-04-10 12:22:17 +0800

    comment out permission for role and permission view page

--- app/policies/role_policy.rb
+++ app/policies/role_policy.rb
@@ -1,5 +1,6 @@
 class RolePolicy < ApplicationPolicy
   def index?
-    current_system_user.is_admin? || current_system_user.role_in_app.has_permission?('role', 'index')
+    #current_system_user.is_admin? || current_system_user.role_in_app.has_permission?('role', 'index')
+    true
   end 
 end
