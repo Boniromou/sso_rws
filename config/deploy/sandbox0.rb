2086ce50f70f9a32234b56dc93072894af88ee36
Ray Chan <ray.chan@laxino.com>
2015-05-04 11:55:11 +0800

    add sandbox0 and production0 related config

---
+++ config/deploy/sandbox0.rb
@@ -0,0 +1,26 @@
+# Define your release to be deployed to integration environment here.
+# Release number for integration environment is supposed to be odd numbers.
+set :branch, 'REL_1_0_0'
+
+# Define your repository server for integration environment here.
+#   production SVN - svn.prod.laxigames.com
+#   development SVN - svn.mo.laxino.com
+set :repo_host, 'svn.mo.laxino.com'
+
+# Define your application servers for integration environment here.
+#   int - Integration
+#   stg - Staging
+#   prd - Production
+role :app, 'ph-snd-sso-app01.laxigames.com'
+
+#role :cronjob_app, 'int-cons-vapp03.rnd.laxino.com'
+
+# Define your database servers for integration environment here.
+# role :db,  "int-cons-db01.rnd.laxino.com", :primary => true
+
+# Define your application cluster with Nginx settings here
+# These variables will be used in generating Nginx/Thin config files
+set :nginx_worker_processes, 2
+set :cluster_port, 10046
+set :virtual_server_name, 'ph-snd-sso-vapp01.laxigames.com'
+set :num_of_servers, 2
--- config/environments/sandbox0.rb
+++ config/environments/sandbox0.rb
@@ -1,4 +1,4 @@
-ApRws::Application.configure do
+SsoRws::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
@@ -64,4 +64,13 @@ ApRws::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.cache_store = :dalli_store,
+                        'snd-cons-vapp01.sandbox.laxigames.com:11211',
+                        {:namespace => 'cirrus_snd',
+                         :expires_in => 1.day,
+                         :socket_timeout => 3,
+                         :compress => true }
+  SITE_DOMAIN = 'laxigames.com'
 end
+
+APP_NAME = 'user'
