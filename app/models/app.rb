b1878d58e2897debd65fb594b15927e657f00683
Ray Chan <ray.chan@laxino.com>
2015-05-20 16:31:25 +0800

    rollback changes for app permission

--- app/models/app.rb
+++ app/models/app.rb
@@ -10,9 +10,9 @@ class App < ActiveRecord::Base
       permissions = role.permissions
       permissions.each do |perm|
         if perm_hash.has_key?(perm.target.to_sym)
-          perm_hash[perm.target.to_sym] << perm.name unless perm_hash[perm.target.to_sym].include? perm.name
+          perm_hash[perm.target.to_sym] << perm.action unless perm_hash[perm.target.to_sym].include? perm.action
         else
-          perm_hash[perm.target.to_sym] = [perm.name]
+          perm_hash[perm.target.to_sym] = [perm.action]
         end
       end
     end
