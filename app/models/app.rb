76197419c4799e18050574d526b8489c06cc3dd6
Ray Chan <ray.chan@laxino.com>
2015-05-20 10:14:18 +0800

    update role and permission fixture, update permission display name

--- app/models/app.rb
+++ app/models/app.rb
@@ -10,9 +10,9 @@ class App < ActiveRecord::Base
       permissions = role.permissions
       permissions.each do |perm|
         if perm_hash.has_key?(perm.target.to_sym)
-          perm_hash[perm.target.to_sym] << perm.action unless perm_hash[perm.target.to_sym].include? perm.action
+          perm_hash[perm.target.to_sym] << perm.name unless perm_hash[perm.target.to_sym].include? perm.name
         else
-          perm_hash[perm.target.to_sym] = [perm.action]
+          perm_hash[perm.target.to_sym] = [perm.name]
         end
       end
     end
