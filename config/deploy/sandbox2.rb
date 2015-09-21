7496f5e6935e0127eee8ffd8fe6371284a333517
Hill Si <hill.si@laxino.com>
2015-09-21 18:34:41 +0800

    add domain constant to database

--- config/deploy/sandbox2.rb
+++ config/deploy/sandbox2.rb
@@ -1,6 +1,6 @@
 # Define your release to be deployed to integration environment here.
 # Release number for integration environment is supposed to be odd numbers.
-set :branch, 'REL_1_1_5'
+set :branch, 'REL_1_1_6'
 
 # Define your repository server for integration environment here.
 #   production SVN - svn.prod.laxigames.com
