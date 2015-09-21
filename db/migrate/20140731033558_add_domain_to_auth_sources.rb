3f03c6ae4d11d85f112d88fb61427c9e68bbce56
Hill Si <hill.si@laxino.com>
2015-09-21 18:34:41 +0800

    add domain constant to database

--- db/migrate/20140731033558_add_domain_to_auth_sources.rb
+++ db/migrate/20140731033558_add_domain_to_auth_sources.rb
@@ -2,6 +2,6 @@ class AddDomainToAuthSources < ActiveRecord::Migration
   def change
     add_column :auth_sources, :domain, :string, :default => "", :null => false
 
-    AuthSource.where(:name => "Laxino LDAP", :host => "10.10.28.91", :port => 389).update_all(:domain => "mo")
+    AuthSource.where(:name => "Laxino LDAP", :host => AUTH_SOURCE_HOST, :port => 389).update_all(:domain => "mo")
   end
 end
