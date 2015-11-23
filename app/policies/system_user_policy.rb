a4ed02992b49379fdc9e640aa79e07aa500d0afb
Ray Chan <ray.chan@laxino.com>
2015-11-23 17:02:53 +0800

    fix typo

--- app/policies/system_user_policy.rb
+++ app/policies/system_user_policy.rb
@@ -28,7 +28,7 @@ class SystemUserPolicy < ApplicationPolicy
       if system_user.is_admin? || system_user.is_internal? || system_user.active_property_ids.include?(INTERNAL_PROPERTY_ID)
         scope.all
       else
-        scope.joins(:properties_system_users).where("properties_system_users.properties.id in (?) AND system_users.auth_source_id = ?", system_user.active_property_ids, AUTH_SOURCE_ID)
+        scope.joins(:properties_system_users).where("properties_system_users.property_id in (?) AND system_users.auth_source_id = ?", system_user.active_property_ids, AUTH_SOURCE_ID)
       end
     end
   end
