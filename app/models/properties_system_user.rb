7847aa40655f5a76dd4c1b361f4eb25ad49a5ee8
Ray Chan <ray.chan@laxino.com>
2015-11-26 11:37:17 +0800

    fix properties_system_user mapping update

--- app/models/properties_system_user.rb
+++ app/models/properties_system_user.rb
@@ -9,14 +9,22 @@ class PropertiesSystemUser < ActiveRecord::Base
 
   def self.update_properties_by_system_user(system_user_id, property_ids)
     transaction do
-      property_ids.each do |property_id|
-        unless exists?(:system_user_id => system_user_id, :property_id => property_id)
-          create!(:system_user_id => system_user_id, :property_id => property_id, :status => true)
-        end
-      end
-
-      suspended_entry = by_system_user_id(system_user_id).exclude_property_ids(property_ids)
-      suspended_entry.update_all(:status => false) unless suspended_entry.blank?
+      property_ids.each { |property_id| grant(system_user_id, property_id) }
+      handle_exclusion(system_user_id, property_ids)
     end
+  end
+
+  private
+  def self.grant(system_user_id, property_id)
+    if exists?(:system_user_id => system_user_id, :property_id => property_id)
+      where(:system_user_id => system_user_id, :property_id => property_id).limit(1).update_all(:status => true)
+    else
+      create!(:system_user_id => system_user_id, :property_id => property_id, :status => true)
+    end
+  end
+
+  def self.handle_exclusion(system_user_id, property_ids)
+    suspended_entry = by_system_user_id(system_user_id).exclude_property_ids(property_ids)
+    suspended_entry.update_all(:status => false) unless suspended_entry.blank?
   end
 end
