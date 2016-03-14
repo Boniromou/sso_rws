0259118e0f76038730f75ccbdb878f463048fd4d
Lansy Peng <lansy.peng@laxino.com>
2016-03-14 18:35:43 +0800

    refactor casino [name,id] show function in change log page

--- lib/rigi/audit.rb
+++ lib/rigi/audit.rb
@@ -5,23 +5,8 @@ module Rigi
 
       def load(file_path=nil)
         file_to_load = file_path || Rails.root.join('config', 'audit.yml')
-        symbolized_h = symbolize_keys(YAML.load_file(file_to_load))
+        symbolized_h = Rigi::Format.symbolize_keys(YAML.load_file(file_to_load))
         Rigi.const_set('AUDIT_CONFIG', symbolized_h[:audit_target])
-      end
-
-      def symbolize_keys(hash)
-        hash.inject({}){|result, (key, value)|
-          new_key = case key
-                    when String then key.to_sym
-                    else key
-                    end
-          new_value = case value
-                      when Hash then symbolize_keys(value)
-                      else value
-                      end
-          result[new_key] = new_value
-          result
-        }
       end
     end
   end
