38b909560f1660eff078128f683e97e8af9142a5
Lansy Peng <lansy.peng@laxino.com>
2016-03-14 11:07:40 +0800

    update migration

--- db/migrate/20160307071046_create_domains_casinos.rb
+++ db/migrate/20160307071046_create_domains_casinos.rb
@@ -5,15 +5,10 @@ class CreateDomainsCasinos < ActiveRecord::Migration
       t.integer :casino_id, :null => false
       t.timestamps
     end
-
-    execute "ALTER TABLE domains_casinos ADD CONSTRAINT fk_DomainsCasinos_DomainId FOREIGN KEY (domain_id) REFERENCES domains(id);"
-    execute "ALTER TABLE domains_casinos ADD CONSTRAINT fk_DomainsCasinos_CasinoId FOREIGN KEY (casino_id) REFERENCES casinos(id);"
     add_index :domains_casinos, ["domain_id", "casino_id"], :unique => true
   end
 
   def down
-  	execute "ALTER TABLE domains_casinos DROP FOREIGN KEY fk_DomainsCasinos_DomainId;"
-    execute "ALTER TABLE domains_casinos DROP FOREIGN KEY fk_DomainsCasinos_CasinoId;"
     drop_table :domains_casinos
   end
 end
--- db/migrate/20160307082511_add_domain_id_to_system_users.rb
+++ db/migrate/20160307082511_add_domain_id_to_system_users.rb
@@ -1,20 +1,9 @@
 class AddDomainIdToSystemUsers < ActiveRecord::Migration
-  def up
-    remove_index :system_users, ["username", "domain"]
-    remove_column :system_users, :domain
-
-    add_column :system_users, :domain_id, :integer
-    execute "ALTER TABLE system_users ADD CONSTRAINT fk_SystemUsers_DomainId FOREIGN KEY (domain_id) REFERENCES domains(id);"
-
-    add_index :system_users, ["username", "domain_id"], :unique => true
+  def up  
+    add_column :system_users, :domain_id, :integer    
   end
 
-  def down
-    execute "ALTER TABLE system_users DROP FOREIGN KEY fk_SystemUsers_DomainId;"
-    remove_index :system_users, ["username", "domain_id"]
+  def down    
     remove_column :system_users, :domain_id
-
-    add_column :system_users, :domain, :string
-    add_index :system_users, ["username", "domain"], :unique => true
   end
 end
