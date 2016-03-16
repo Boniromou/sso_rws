b53b789193c7ecf29ce5d9dda28b67a7bf428b62
Lansy Peng <lansy.peng@laxino.com>
2016-03-16 11:10:49 +0800

    update db schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160307082511) do
+ActiveRecord::Schema.define(:version => 20160314021508) do
 
   create_table "app_system_users", :force => true do |t|
     t.integer  "system_user_id", :null => false
