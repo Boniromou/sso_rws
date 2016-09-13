edb910dce81749b44754d11a8db0ad53acc0c2d4
Xiaopan Yun <xiaopan.yun@laxino.com>
2016-09-13 17:31:38 +0800

    deploy sandbox3 change config

--- config/deploy/sandbox3.rb
+++ config/deploy/sandbox3.rb
@@ -1,17 +1,17 @@
 # Define your release to be deployed to integration environment here.
 # Release number for integration environment is supposed to be odd numbers.
-set :branch, 'master'
+set :branch, 'CBG_0_4_5'
 
 # Define your repository server for integration environment here.
 #   production SVN - svn.prod.laxigames.com
 #   development SVN - svn.mo.laxino.com
-set :repo_host, 'mo-prd-cbms-app01.laxigames.local'
+set :repo_host, 'mo-prd-cbms-app01.gb.laxigames.local'
 
 # Define your application servers for integration environment here.
 #   int - Integration
 #   stg - Staging
 #   prd - Production
-role :app, 'mo-snd-sso-app01.laxigames.local'
+role :app, 'mo-snd-sso-app01.lb.laxigames.local'
 
 #role :cronjob_app, 'int-cons-vapp03.rnd.laxino.com'
 
@@ -22,6 +22,6 @@ role :app, 'mo-snd-sso-app01.laxigames.local'
 # These variables will be used in generating Nginx/Thin config files
 set :nginx_worker_processes, 2
 set :cluster_port, 10046
-set :virtual_server_name, 'mo-snd-sso-vapp01.laxigames.local'
+set :virtual_server_name, 'mo-snd-sso-vapp01.lb.laxigames.local'
 set :num_of_servers, 2
 
