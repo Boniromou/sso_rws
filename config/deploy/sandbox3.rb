c0f11d132430eaa9fd03d4e5f0c4eb7a1bd463da
Xiaopan Yun <xiaopan.yun@laxino.com>
2016-12-13 18:12:20 +0800

    deploy sandbox3: cache username with domain

--- config/deploy/sandbox3.rb
+++ config/deploy/sandbox3.rb
@@ -1,6 +1,6 @@
 # Define your release to be deployed to integration environment here.
 # Release number for integration environment is supposed to be odd numbers.
-set :branch, 'CBG_0_4_6'
+set :branch, 'CBG_0_4_8'
 
 # Define your repository server for integration environment here.
 #   production SVN - svn.prod.laxigames.com
