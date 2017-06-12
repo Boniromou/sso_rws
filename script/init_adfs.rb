037fafd40d3ded7d9557f25bd6927a85d2d31b16
Hill Si <hill.si@laxino.com>
2017-06-12 12:30:16 +0800

    add script init_adfs

---
+++ script/init_adfs.rb
@@ -0,0 +1,38 @@
+require 'yaml'
+require 'json'
+require 'fileutils'
+require 'sequel'
+require 'logger'
+
+if ARGV.length != 2
+  puts "Usage: ruby script/init_adfs.rb <env> <file_name>"
+  puts "Example: ruby script/init_adfs.rb development config/adfs.yml"
+  Process.exit
+end
+Dir[File.expand_path("utils/*.rb",File.dirname( __FILE__))].each { |file| require file }
+db = Database.connect(ARGV[0])
+configs = YAML.load_file(ARGV[1])[ARGV[0]]
+
+p configs
+
+print "Overwrite? (Y/n): "
+prompt = STDIN.gets.chomp
+
+if prompt.casecmp('Y') == 0
+  configs.each do |_, config|
+    auth_src_dtl = db[:auth_source_details].where(:name => config['auth_source_detail']['name'])
+    db.transaction do
+      auth_src_id = db[:auth_sources].insert(:token => config['auth_source']['token'],
+                                             :type => config['auth_source']['type'])
+      if auth_src_dtl.first
+        db[:auth_sources].where(:id => auth_src_id).update(:auth_source_detail_id => auth_src_dtl.first[:id])
+      else
+        auth_src_dtl_id = db[:auth_source_details].insert(:name => config['auth_source_detail']['name'],
+                                                          :data => config['auth_source_detail']['data'].to_json,
+                                                          :created_at => Time.now.utc,
+                                                          :updated_at => Time.now.utc)
+        db[:auth_sources].where(:id => auth_src_id).update(:auth_source_detail_id => auth_src_dtl_id)
+      end
+    end
+  end
+end
