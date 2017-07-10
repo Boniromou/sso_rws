0ee9740e19f5e0cfd8a32614491d535656d8bcee
Xiaopan Yun <xiaopan.yun@laxino.com>
2017-07-10 11:13:05 +0800

    change int, stg deploy config

--- config/deploy/integration0.rb
+++ config/deploy/integration0.rb
@@ -1,6 +1,6 @@
 # Define your release to be deployed to integration environment here.
 # Release number for integration environment is supposed to be odd numbers.
-set :branch, 'hq'
+set :branch, 'integration'
 
 # Define your repository server for integration environment here.
 #   production SVN - svn.prod.laxigames.com
--- config/deploy/staging0.rb
+++ config/deploy/staging0.rb
@@ -1,6 +1,6 @@
 # Define your release to be deployed to integration environment here.
 # Release number for integration environment is supposed to be odd numbers.
-set :branch, 'old_stg_deploy'
+set :branch, 'staging'
 
 # Define your repository server for integration environment here.
 #   production SVN - svn.prod.laxigames.com
@@ -11,7 +11,7 @@ set :repo_host, 'svn.mo.laxino.com'
 #   int - Integration
 #   stg - Staging
 #   prd - Production
-role :app, 'mo-stg-sso-app01.rnd.laxino.com'
+role :app, 'hq-stg-sso-app01.laxino.local'
 
 #role :cronjob_app, 'int-cons-vapp03.rnd.laxino.com'
 
@@ -22,7 +22,7 @@ role :app, 'mo-stg-sso-app01.rnd.laxino.com'
 # These variables will be used in generating Nginx/Thin config files
 set :nginx_worker_processes, 2
 set :cluster_port, 10042
-set :virtual_server_name, 'mo-stg-sso-vapp01.rnd.laxino.com'
+set :virtual_server_name, 'hq-stg-sso-vapp01.laxino.local'
 set :num_of_servers, 2
 set :keep_releases, 2
 after 'deploy', 'deploy:cleanup'
