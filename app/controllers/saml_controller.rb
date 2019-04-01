4fbd8d82aa753d9f1cee9a3d66d8f3bcfdc5e077
xiaopan.yun <xiaopan.yun@gamesourcecloud.com>
2019-04-01 10:19:59 +0000

    Revert "fix saml metadata"
    This reverts commit a835b95888e850095ee1cacc62c7a36fc763463b.

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -29,7 +29,7 @@ class SamlController < ApplicationController
   end
 
   def metadata
-    settings = get_saml_settings(true)
+    settings = get_saml_settings
     meta = OneLogin::RubySaml::Metadata.new
     render :xml => meta.generate(settings, true)
   end
@@ -109,9 +109,9 @@ class SamlController < ApplicationController
     casino_ids.collect {|casino_id| casino_id.delete('casinoid').to_i}
   end
 
-  def get_saml_settings(is_metadata = false)
+  def get_saml_settings
     settings = AuthSource.find_by_token(get_client_ip).get_saml_settings(get_url_base, app_name, is_second_authorize?)
-    if is_metadata
+    if !app_name
       settings.assertion_consumer_service_url = get_url_base + "/saml/acs"
       settings.assertion_consumer_logout_service_url = get_url_base + "/saml/logout"
     end
