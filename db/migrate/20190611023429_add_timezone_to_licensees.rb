d9b0ed44f2b2ec0f036849cf966be445f322d29e
xiaopan.yun <xiaopan.yun@gamesourcecloud.com>
2019-06-24 03:38:18 +0000

    fix licensee 1000 timezone default value

--- db/migrate/20190611023429_add_timezone_to_licensees.rb
+++ db/migrate/20190611023429_add_timezone_to_licensees.rb
@@ -1,6 +1,6 @@
 class AddTimezoneToLicensees < ActiveRecord::Migration
   def change
     add_column :licensees, :timezone, :string, :limit => 45
-    execute "UPDATE licensees SET timezone = '+08:00' WHERE id = 1000"
+    execute "UPDATE licensees SET timezone = 'Asia/Macao' WHERE id = 1000"
   end
 end
