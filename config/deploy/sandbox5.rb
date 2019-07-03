c73d3596ba1d4235dbf4676dbb0d83374203416e
xiaopan.yun <xiaopan.yun@gamesourcecloud.com>
2019-07-03 02:57:29 +0000

    fix snd5 deploy config

--- config/deploy/sandbox5.rb
+++ config/deploy/sandbox5.rb
@@ -1,11 +1,11 @@
 # Define your release to be deployed to integration environment here.
 # Release number for integration environment is supposed to be odd numbers.
-set :branch, 'CBG_0_6_3'
+set :branch, 'CBG_0_6_4'
 
 # Define your repository server for integration environment here.
 #   production SVN - svn.prod.laxigames.com
 #   development SVN - svn.mo.laxino.com
-set :repo_host, 'prd-cbms-app01.prod.laxigames.com'
+set :repo_host, 'svn.prod.laxigames.com'
 set :user, 'swe'
 set :group, 'swe'
 
