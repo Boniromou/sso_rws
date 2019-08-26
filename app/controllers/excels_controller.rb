50b19213c9a9d774f3eafcf37c273b08b7010f19
Xiaopan Yun <xiaopan.yun@gamesourcecloud.com>
2019-08-26 03:19:03 +0000

    fix export change log policy

--- app/controllers/excels_controller.rb
+++ app/controllers/excels_controller.rb
@@ -8,14 +8,14 @@ class ExcelsController < ApplicationController
   TIP_FORMAT = Spreadsheet::Format.new :size => 11, :horizontal_align => :left, :vertical_align => :centre
   TITLE_FORMAT = Spreadsheet::Format.new :weight => :bold, :size => 9, :horizontal_align => :centre, :vertical_align => :centre, :pattern_fg_color => :silver  , :pattern => 1
   TEXT_FORMAT = Spreadsheet::Format.new :size => 9, :horizontal_align => :centre, :vertical_align => :centre
-  
+
   def create_system_user_log
-    logs = SystemUserChangeLog.by_action('create')
+    logs = get_create_user_logs
     page_to_excel("SSO_CreateSystemUserLog") do |sheet|
       sheet.row(0).default_format = HEAD_FORMAT
       sheet.row(1).default_format = TIP_FORMAT
       sheet.row(0).push("Create System User Log")
-      sheet.row(1).concat(["Last updated at",  format_time(Time.now)]) 
+      sheet.row(1).concat(["Last updated at",  format_time(Time.now)])
 
       title = %W( System\ User Casinos Action Action\ at Action\ by Casinos )
       title.count.times.each { |i|  sheet.row(3).set_format(i, TITLE_FORMAT) }
@@ -35,12 +35,12 @@ class ExcelsController < ApplicationController
   end
 
   def system_user_log
-    logs = SystemUserChangeLog.by_action('edit_role')
+    logs = get_edit_role_logs
     page_to_excel("SSO_SystemUserLog") do |sheet|
       sheet.row(0).default_format = HEAD_FORMAT
       sheet.row(1).default_format = TIP_FORMAT
       sheet.row(0).push("System User Log")
-      sheet.row(1).concat(["Last updated at",  format_time(Time.now)]) 
+      sheet.row(1).concat(["Last updated at",  format_time(Time.now)])
 
       title = %W( Action\ by Casinos Action\ at Action System\ User  Casinos System From To )
       title.count.times.each { |i|  sheet.row(3).set_format(i, TITLE_FORMAT) }
@@ -63,12 +63,12 @@ class ExcelsController < ApplicationController
   end
 
   def login_history
-    historires =  LoginHistory.all.as_json(:include => ['system_user', 'domain', 'app'])
+    historires =  get_login_histories
     page_to_excel("SSO_LoginHistory") do |sheet|
       sheet.row(0).default_format = HEAD_FORMAT
       sheet.row(1).default_format = TIP_FORMAT
       sheet.row(0).push("Login History")
-      sheet.row(1).concat(["Last updated at",  format_time(Time.now)]) 
+      sheet.row(1).concat(["Last updated at",  format_time(Time.now)])
 
       title = %W(System\ User Casinos System Login\ Time )
       title.count.times.each { |i|  sheet.row(3).set_format(i, TITLE_FORMAT) }
@@ -76,10 +76,10 @@ class ExcelsController < ApplicationController
       historires.each_with_index do |history, index|
         sheet.row(4 + index).default_format = TEXT_FORMAT
         sheet.row(4 + index).concat([
-          "#{history['system_user']['username']}@#{history['domain']['name']}",
-          casino_id_names_format(history['detail']['casino_id_names']),
-          history['app']['name'].titleize,
-          format_time(history['sign_in_at'])
+          "#{history.system_user.username}@#{history.domain.name}",
+          casino_id_names_format(history.detail['casino_id_names']),
+          history.app.try(:name).to_s.titleize,
+          format_time(history.sign_in_at)
         ])
       end
     end
@@ -94,5 +94,28 @@ class ExcelsController < ApplicationController
     xls.write string_io
     send_data string_io.string, :filename => "#{filename}.xls", :type =>  "application/vnd.ms-excel"
   end
-  
+
+  def get_create_user_logs
+    start_time = parse_date(params[:export_start_time])
+    end_time = parse_date(params[:export_end_time], true)
+    policy_scope(SystemUserChangeLog.by_action('create').search_query('', start_time, end_time))
+  end
+
+  def get_edit_role_logs
+    start_time = parse_date(params[:export_start_time])
+    end_time = parse_date(params[:export_end_time], true)
+    policy_scope(SystemUserChangeLog.by_action('edit_role').search_query(params[:export_username], start_time, end_time))
+  end
+
+  def get_login_histories
+    start_time, end_time, remark = format_time_range(params[:export_start_time], params[:export_end_time], SEARCH_RANGE_FOR_LOGIN_HISTORY)
+    return [] if start_time.nil? || end_time.nil?
+    if params[:export_username].present?
+      system_user = SystemUser.find_by_username_with_domain(params[:export_username])
+      return [] unless system_user
+      system_user_id = system_user.id
+      domain_id = system_user.domain_id
+    end
+    policy_scope(LoginHistory.search_query(system_user_id, domain_id, params[:export_app_id], start_time, end_time))
+  end
 end
--- app/controllers/login_histories_controller.rb
+++ app/controllers/login_histories_controller.rb
@@ -18,17 +18,21 @@ class LoginHistoriesController < ApplicationController
   private
 
   def handle_search_with_result
+    @start_time_text = params[:start_time]
+    @end_time_text = params[:end_time]
+    @username = params[:username]
+    @app_id = params[:app_id] unless params[:app_id].blank? || params[:app_id] == "all"
     start_time, end_time, remark = format_time_range(params[:start_time], params[:end_time], SEARCH_RANGE_FOR_LOGIN_HISTORY)
     if start_time.nil? && end_time.nil?
       @search_error = I18n.t("login_history.search_range_error", :config_value => SEARCH_RANGE_FOR_LOGIN_HISTORY)
     else
       if remark
         @search_time_range_remark = I18n.t("login_history.search_range_remark", :config_value => SEARCH_RANGE_FOR_LOGIN_HISTORY)
-        @default_start_time = format_date(start_time)
-        @default_end_time = format_date(end_time - 1.days)
+        @start_time_text = format_date(start_time)
+        @end_time_text = format_date(end_time - 1.days)
       end
-      if params[:username].present?
-	      system_user = SystemUser.find_by_username_with_domain(params[:username])
+      if @username.present?
+	      system_user = SystemUser.find_by_username_with_domain(@username)
 	      if system_user.present?
 		      system_user_id = system_user.id
 		      domain_id = system_user.domain_id
@@ -38,9 +42,8 @@ class LoginHistoriesController < ApplicationController
 	    end
 
 	    if @search_error.blank?
-	      app_id = params[:app_id] unless params[:app_id].blank? || params[:app_id] == "all"
-		    @login_histories = policy_scope(LoginHistory.search_query(system_user_id, domain_id, app_id, start_time, end_time)).as_json(:include => ['system_user', 'domain', 'app'])
-		  end
+		    @login_histories = policy_scope(LoginHistory.search_query(system_user_id, domain_id, @app_id, start_time, end_time)).as_json(:include => ['system_user', 'domain', 'app'])
+      end
     end
   end
 end
