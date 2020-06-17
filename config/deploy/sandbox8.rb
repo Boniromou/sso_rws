4cef6f75d44216eb3b5edbd1cf05cf312d6fc441
Xiaopan Yun <xiaopan.yun@gamesourcecloud.com>
2020-06-17 03:31:12 +0000

    add snd8 deploy config

---
+++ config/deploy/sandbox8.rb
@@ -0,0 +1,33 @@
+# Define your release to be deployed to integration environment here.
+# Release number for integration environment is supposed to be odd numbers.
+set :branch, 'CBG_0_6_12'
+
+# Define your repository server for integration environment here.
+#   production SVN - svn.prod.laxigames.com
+#   development SVN - svn.mo.laxino.com
+set :user, "swe"
+set :group, "swe"
+set :repo_host, 'svn.mo.laxino.com'
+
+# Define your application servers for integration environment here.
+#   int - Integration
+#   stg - Staging
+#   prd - Production
+role :app, '10.10.45.111'
+
+role :cronjob_app, '10.10.45.111'
+
+#role :cronjob_app, 'int-cons-vapp03.rnd.laxino.com'
+
+# Define your database servers for integration environment here.
+# role :db,  "int-cons-db01.rnd.laxino.com", :primary => true
+
+# Define your application cluster with Nginx settings here
+# These variables will be used in generating Nginx/Thin config files
+set :nginx_worker_processes, 2
+set :cluster_port, 10046
+set :virtual_server_name, '10.10.45.111'
+set :num_of_servers, 2
+
+set :keep_releases, 2
+after 'deploy', 'deploy:cleanup'
