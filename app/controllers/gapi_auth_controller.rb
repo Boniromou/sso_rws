ef84c5e351db26c61101db6672146e3173177290
Xiaopan Yun <xiaopan.yun@gamesoucecloud.com>
2021-06-11 09:27:21 +0000

    fix second authorize

--- app/controllers/gapi_auth_controller.rb
+++ app/controllers/gapi_auth_controller.rb
@@ -4,19 +4,16 @@ class GapiAuthController < ApplicationController
 
   def new
     check_login_type!('gapi')
-    @message_id = SecureRandom.hex
-    session[:message_id] = @message_id
     @app_name = params[:app_name]
     auth_source = AuthSource.find_by_token(get_client_ip)
     @g_clien_id = auth_source.auth_source_detail['data']['client_id']
   end
 
   def create
-    check_message_id
     auth_source = AuthSource.find_by_token(get_client_ip)
-    system_user = auth_source.authorize!(params[:system_user][:username], params[:app_name], auth_info['casino_id'], auth_info['permission'])
+    system_user = auth_source.authorize!(params[:username], params[:app_name], auth_info['casino_id'], auth_info['permission'])
     Rails.logger.info 'Authorize successfully.'
-    write_authorize_cookie({error_code: 'OK', error_message: 'Authorize successfully.', authorized_by: params[:system_user][:username], authorized_at: Time.now})
+    write_authorize_cookie({error_code: 'OK', error_message: 'Authorize successfully.', authorized_by: params[:username], authorized_at: Time.now})
     render :json => {error_code: 'OK', error_msg: 'Request is now completed', callback_url: auth_info['callback_url']}
   rescue Rigi::InvalidLogin => e
     @app_name = params[:app_name]
@@ -25,12 +22,5 @@ class GapiAuthController < ApplicationController
     Rails.logger.error "Authorize failed: #{e.error_message}"
     write_authorize_cookie({error_code: e.class.name.demodulize, error_message: e.error_message})
     render :json => {error_code: 'InvalidAuthorize', error_msg: 'Authorize failed.', callback_url: auth_info['callback_url']}
-  end
-
-  protected
-  def check_message_id
-    message_id = session[:message_id]
-    session.delete(:message_id)
-    raise Rigi::DuplicateAuthorize.new('Duplicate authorization.') if message_id != params[:message_id]
   end
 end
--- app/models/gapi.rb
+++ app/models/gapi.rb
@@ -4,15 +4,11 @@ class Gapi < AuthSource
   end
 
   def get_auth_url(app_name)
-    "#{URL_BASE}/gapi/new?app_name=#{app_name}&second_authorize=true"
+    "#{URL_BASE}/gapi_auth/new?app_name=#{app_name}&second_authorize=true"
   end
 
   def authenticate!(username, app_name, casino_ids)
-    system_user = SystemUser.find_by_username_with_domain(username)
-    if system_user.nil?
-      Rails.logger.error "SystemUser[username=#{username}] Login failed. Not a registered account"
-      raise Rigi::InvalidLogin.new("alert.invalid_login")
-    end
+    system_user = valid_before_login!(username)
     casino_ids = system_user.domain.get_casino_ids & casino_ids
     super(username, app_name, SystemUser::ACTIVE, casino_ids)
   end
@@ -21,10 +17,20 @@ class Gapi < AuthSource
     SystemUser.register_without_check!(username, domain)
   end
 
-  def authorize!(usernam, app_name, casino_id, permission)
-    valid_before_login!(username)
+  def authorize!(username, app_name, casino_id, permission)
+    system_user = valid_before_login!(username)
     system_user = SystemUser.find_by_username_with_domain(username)
     system_user = authenticate_without_cache!(username, app_name, 'active', [casino_id])
     system_user.authorize!(app_name, casino_id, permission)
+  end
+
+  private
+  def valid_before_login!(username)
+    system_user = SystemUser.find_by_username_with_domain(username)
+    if system_user.nil?
+      Rails.logger.error "SystemUser[username=#{username}] Login failed. Not a registered account"
+      raise Rigi::InvalidLogin.new("alert.invalid_login")
+    end
+    system_user
   end
 end
