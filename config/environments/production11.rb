ad36afdf56f34690a6de22600865275dee6cf6e3
RandyTai <randy.tai@gamessourcecloud.com>
2022-01-26 15:47:12 +0800

    fix config

--- config/environments/production11.rb
+++ config/environments/production11.rb
@@ -65,10 +65,10 @@ SsoRws::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.cache_store = :dalli_store,
-                        'esb-s3-vapp01.prd11.gamesource.local:7000',
-                        'esb-s3-vapp01.prd11.gamesource.local:7001',
-                        'esb-s3-vapp02.prd11.gamesource.local:7000',
-                        'esb-s3-vapp02.prd11.gamesource.local:7001',
+                        'esb-s3-vapp01.prd11.gs.local:7000',
+                        'esb-s3-vapp01.prd11.gs.local:7001',
+                        'esb-s3-vapp02.prd11.gs.local:7000',
+                        'esb-s3-vapp02.prd11.gs.local:7001',
                         {:namespace => 'cirrus_prd',
                          :expires_in => 1.day,
                          :socket_timeout => 3,
