a1384d93155a997453f359b3a6abb6c1bee2aab5
mark.chan <laxino@localhost.localdomain>
2022-01-26 02:39:49 -0500

    typo

--- config/deploy/production11.rb
+++ config/deploy/production11.rb
@@ -1,6 +1,6 @@
 # Define your release to be deployed to integration environment here.
 # Release number for integration environment is supposed to be odd numbers.
-set :branch, 'CBG_0_7_8'
+set :branch, 'CBG_0_8_0'
 
 # Define your repository server for integration environment here.
 #   production SVN - svn.prod.laxigames.com
